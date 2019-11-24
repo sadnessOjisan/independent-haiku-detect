6a4f4b0d4154fdd7a3353e4df8f51ad1a0db616c
sadnessOjisan <sadness.ojisan@gmail.com>
2019-11-24 13:48:25 +0900

    ファイル名を受け取るとソースコードをトークン化する (#2)
    * update: get values
    
    * update: use file

--- main.js
+++ main.js
@@ -1,17 +1,11 @@
-const result = require("@babel/parser").parse(
-  `
-class Hoge {
-    constructor() {
-      alert("hey men");
-    }
-  }
-  
-`,
-  {
+const fs = require("fs");
+
+fs.readFile("./example/example1.js", "utf-8", (err, data) => {
+  if (err) throw err; // 例外発生時の処理
+  const result = require("@babel/parser").parse(data, {
     sourceType: "module",
+    tokens: true
+  });
 
-    plugins: []
-  }
-);
-
-console.log(result);
+  console.log(result.tokens.map(t => t.value));
+});
