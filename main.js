358c17ae4511318a229a1868ee165e9a835a26f6
sadness_ojisan <sadness.ojisan@gmail.com>
2019-12-15 12:59:49 +0900

    wip: plugin最新化したのちに修正

--- main.js
+++ main.js
@@ -1,11 +1,8 @@
 const fs = require("fs");
-
-fs.readFile("./example/example1.js", "utf-8", (err, data) => {
-  if (err) throw err; // 例外発生時の処理
-  const result = require("@babel/parser").parse(data, {
-    sourceType: "module",
-    tokens: true
-  });
-
-  console.log(result.tokens.map(t => t.value));
+const data = fs.readFileSync("./example/example1.js", "utf-8");
+const result = require("@babel/parser").parse(data, {
+  sourceType: "module",
+  tokens: true
 });
+
+console.log(result.tokens.map(t => t.value));
